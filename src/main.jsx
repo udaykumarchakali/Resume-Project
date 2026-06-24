3737c989198106fb72603649a48a9b360e08645a
Uday <json90327@gmail.com.id>
2026-06-24 22:16:55 +0530

    "laxman"

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
-import App from './App.jsx'
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-)
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,53 +1,34 @@
 import hero from "../assets/YTDown_YouTube_Shoes-by-2GO-Theatrical-Ad-Film-Commerci_Media_geFi-ZpN2ZM_002_720p.mp4";
-
 import Products from "../Components/Products";
-
 import { useNavigate } from "react-router-dom";
-import '../Components/Navbar.css'
-
+import "../Components/Navbar.css";
 
 const Home = () => {
-
   const navigate = useNavigate();
+
   return (
-    <div>
-
+    <>
       <div className="hero-section">
-
-        <video
-          autoPlay
-          loop
-          muted
-          className="hero-video"
-        >
-          <source
-            src={hero}
-            type="video/mp4"
-          />
+        <video autoPlay loop muted className="hero-video">
+          <source src={hero} type="video/mp4" />
         </video>
 
-        <div
-    className="position-absolute top-50 start-50 translate-middle text-center text-white"
-  >
-    <h1 className="display-2 fw-bold">
-      New Fashion Collection
-    </h1>
+        <div className="hero-content">
+          <h1>New Fashion Collection</h1>
 
-    <p className="fs-4">
-      Trendy Dresses For Every Style
-    </p>
+          <p>Trendy Dresses For Every Style</p>
 
-    <button className="btn4 btn-lg mt-3"
-    onClick={() => navigate("/productsPage")}>
-      Shop Now
-    </button>
-  </div>
-
+          <button
+            className="btn4"
+            onClick={() => navigate("/productsPage")}
+          >
+            Shop Now
+          </button>
+        </div>
       </div>
-<Products/>
 
-
-    </div>
+      <Products />
+    </>
   );
 };
 
